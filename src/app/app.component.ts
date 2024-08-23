1c61bfd5e4d5ecdee96ce05c68e3ac81ecba6592
Dota43ver <facundoagueroc@gmail.com>
2024-08-23 14:42:58 -0300

    inicio de formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EstadosService } from './services/estados/estados.service';
+import { ContactoService } from './services/contacto/contacto.service';
+import { PaisesService } from './services/paises/paises.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'contactocrud';
+export class AppComponent implements OnInit {
+  personaForm: FormGroup | undefined;
+
+  constructor(public fb: FormBuilder, public estadosServices: EstadosService, public contactoServices: ContactoService, public paisesServices: PaisesService) {
+
+  }
+  ngOnInit(): void {
+    this.personaForm = this.fb.group({    
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      pais: ['', Validators.required],
+      estado: ['', Validators.required],
+      numero: ['', Validators.required],
+    });
+  }
+
+  guardar():void{
+
+  }
 }
